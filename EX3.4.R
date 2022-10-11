a8c31390356c22adc0629cae7f8c0192c6eae7e8
LiamPhan17 <liam.phan2000@gmail.com>
2022-10-11 12:06:00 +0200

    Ex3.4

--- EX3.4.R
+++ EX3.4.R
@@ -6,12 +6,24 @@ library(tidyverse)
 library(esquisse)
 library(plyr)
 library(ggplot2)
+library(cowplot)
+library(naniar) #for NA exploration
+library(sp) #spatial data
+library(ggmap) #for map
+library(osmdata) #openstreetmap
+library(reshape2)
 
 # EX 3.4 ------------
 
 # Loading Data
 
 Laptop_Sales_Data <- fread("DATA/LaptopSales_red.csv")
+
+summary(Laptop_Sales_Data)
+
+str(Laptop_Sales_Data)
+
+gg_miss_var(Laptop_Sales_Data, show_pct = TRUE)
 
 ## a.Price Question ------------------
 
@@ -23,9 +35,16 @@ str(Retail_Price_and_Dates)
 
 Retail_Price_and_Dates <- na.omit(Retail_Price_and_Dates)
 
+#### Histogram of the Retail Price of Computer In 2018
+
+hist(Retail_Price_and_Dates$Retail.Price, main = "Histogram of the Retail Price of Computer In 2018", xlab = "Price", ylab = "Frequency", col = "#1c6155")
+
+# Actual price
 Max_Date_Retail <- max(Retail_Price_and_Dates$Date)
 
 Actual_Price <- Retail_Price_and_Dates[Date %in% Max_Date_Retail, ]
+
+Actual_Price
 
 ### ii.
 
@@ -33,31 +52,33 @@ Retail_Price_and_Dates_Month <- Retail_Price_and_Dates[, mean(Retail.Price), by 
 Retail_Price_and_Dates_Week <- Retail_Price_and_Dates[, mean(Retail.Price), by = floor_date(Date,unit = "week")]
 Retail_Price_and_Dates_Day <- Retail_Price_and_Dates[, mean(Retail.Price), by = floor_date(Date,unit = "day")]
 
-# By Month
+# Retail Price By Month
 ggplot(Retail_Price_and_Dates_Month) +
  aes(x = floor_date, weight = V1) +
- geom_bar(fill = "#112446") +
+ geom_bar(fill = "#1c6155") +
  labs(x = "Month", 
  y = "Mean Price", title = "Retail Price of Computer", subtitle = "Aggregated By Month", caption = "LaptopSalesRed.csv") +
  theme_light()
 
-# By Week
+# Retail Price By Week
 ggplot(Retail_Price_and_Dates_Week) +
   aes(x = floor_date, weight = V1) +
-  geom_bar(fill = "#112446") +
+  geom_bar(fill = "#1c6155") +
   labs(x = "Week", 
        y = "Mean Price", title = "Retail Price of Computer", subtitle = "Aggregated By Week", caption = "LaptopSalesRed.csv") +
   theme_light()
 
-# By Day
+# Retail Price By Day
 ggplot(Retail_Price_and_Dates_Day) +
   aes(x = floor_date, weight = V1) +
-  geom_bar(fill = "#112446") +
+  geom_bar(fill = "#1c6155") +
   labs(x = "Day", 
        y = "Mean Price", title = "Retail Price of Computer", subtitle = "Aggregated By Day", caption = "LaptopSalesRed.csv") +
   theme_light()
 
 ### iii.
+
+
 
 ### iv.
 
